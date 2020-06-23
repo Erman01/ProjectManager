7634daef3b76f37ec2be90f641be04cfefeebe1e
Erman Kemanci <ekemanci01@hotmail.com>
2020-06-23 15:58:49 +0100

    Made Initial Settings

---
+++ ProjectManager/ProjectManager.MVCUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManager.MVCUI.Startup))]
+namespace ProjectManager.MVCUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
